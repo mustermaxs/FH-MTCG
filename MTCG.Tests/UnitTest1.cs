1127337d86b1512f7491484838c333d93baadaf2
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-11-29 12:47:31 +0100

    fixed unit test bug

--- MTCG.Tests/UnitTest1.cs
+++ MTCG.Tests/UnitTest1.cs
@@ -50,6 +50,8 @@ namespace UnitTests.Routing
         // [TestCase("/mtcg/user/{username:alpha}/", "^mtcg/user/(?<username>[a-zA-Z-]+)", true, HTTPMethod.PUT)]
         // [TestCase("/mtcg/security/{token:alphanum}/", "^mtcg/security/(?<token>[a-zA-Z0-9-]+)", true, HTTPMethod.POST)]
         // [TestCase("/mtcg/route/without/params/", "/mtcg/route/without/params/", true, HTTPMethod.DELETE)]
+
+
         [TestCase("/mtcg/user/{userid:int}/", "mtcg/user/23", true, HTTPMethod.GET)]
         [TestCase("/mtcg/user/{username:alpha}/", "mtcg/user/maximilian", true, HTTPMethod.PUT)]
         [TestCase("/mtcg/security/{token:alphanum}/", "mtcg/security/testtoken12398sdfkj98", true, HTTPMethod.POST)]
@@ -72,17 +74,17 @@ namespace UnitTests.Routing
                 registry.RegisterGet(routeTemplate);
                 ResolvedUrl result = registry.MapRequest(requestedUrl, method);
 
-                Assert.IsTrue(result?.IsRouteRegistered ?? false, $"{registry.GetType().Name} wasn't able to map the requested route.\n" +
+                Assert.IsTrue(result?.IsRouteRegistered, $"{registry.GetType().Name} wasn't able to map the requested route.\n" +
                 $"Requested Url: {requestedUrl}\n" +
                 $"Template:     {routeTemplate}\n" +
                 $"Pattern:      {pattern}");
             }
         }
 
-        [TestCase("/mtcg/user/{userid:int}/", "mtcg/user/abc23", true, HTTPMethod.GET)]
-        [TestCase("/mtcg/user/{username:alpha}/", "mtcg/wrong/maximilian234", true, HTTPMethod.PUT)]
-        [TestCase("/mtcg/security/{token:alphanum}/", "mtcg/insecurity/testtoken12398sdfkj98", true, HTTPMethod.POST)]
-        [TestCase("/mtcg/route/without/params/", "/mtcg/route/with/params/123/", true, HTTPMethod.DELETE)]
+        [TestCase("/mtcg/user/{userid:int}/", "mtcg/user/abc23", false, HTTPMethod.GET)]
+        [TestCase("/mtcg/user/{username:alpha}/", "mtcg/wrong/maximilian234", false, HTTPMethod.PUT)]
+        [TestCase("/mtcg/security/{token:alphanum}/", "mtcg/insecurity/testtoken12398sdfkj98", false, HTTPMethod.POST)]
+        [TestCase("/mtcg/route/without/params/", "/mtcg/route/with/params/123/", false, HTTPMethod.DELETE)]
         public void RouteRegistry_CantFindRegisteredAndRequestedRoute(string routeTemplate, string requestedUrl, bool foundRoute, HTTPMethod method)
         {
             string pattern = parser.ReplaceTokensWithRegexPatterns(routeTemplate);
@@ -90,10 +92,36 @@ namespace UnitTests.Routing
             ResolvedUrl result = registry.MapRequest(requestedUrl, method);
             Assert.IsFalse(result.IsRouteRegistered,
             $"---------------------------------------------------------------------\n" +
-            $"{registry.GetType().Name} wasn't able to map the requested route.\n" +
+            $"{registry.GetType().Name} should not have found the requested route.\n" +
             $"Url:      {requestedUrl}" +
             $"\nTemplate: {routeTemplate}" +
             $"\nPattern:  {pattern}");
         }
     }
+
+    // [TestFixture]
+    // public class MTCG_HttpServer
+    // {
+    //     HttpServer? server;
+
+    //     [SetUp]
+    //     public void SetUp()
+    //     {
+    //         server = new HttpServer();
+    //     }
+
+    //     [Test]
+    //     public void HttpServer_ServerStarts()
+    //     {
+    //         Console.WriteLine("start server");
+    //         server.Run();
+    //         Assert.IsTrue(server.Active, $"Server didn't start.");
+    //         server.Stop();
+    //     }
+
+    //     [TearDown]
+    //     public void HttpServer_StopServer()
+    //     {
+    //     }
+    // }
 }

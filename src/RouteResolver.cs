8fae80d53ea0dff1cc74c546454785421b668677
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-11-29 14:03:27 +0100

    added comments to RouteResolver and IUrlParser

--- src/RouteResolver.cs
+++ src/RouteResolver.cs
@@ -5,6 +5,11 @@ using System.Text.RegularExpressions;
 
 namespace MTCG;
 
+/// <summary>
+/// Wrapper to store named params and theirs
+/// corresponding values (among other things).
+/// </summary>
+
 public class ResolvedUrl
 {
     private Dictionary<string, string> urlParams = new Dictionary<string, string>();
@@ -41,6 +46,14 @@ public class ResolvedUrl
         get => this.rawUrl;
     }
 
+    /// <summary>
+    /// Gets value of named parameter by parameter name and tries to convert it to 
+    /// expected datatype.
+    /// </summary>
+    /// <returns>
+    /// Value of named parameter. Throws KeyNotFoundException if it doesn't exist. 
+    /// Throws InvalidOperationException if it's tried to be cast to invalid datatype
+    /// </returns>
     public T GetParam<T>(string key)
     {
         string? value;
@@ -62,14 +75,20 @@ public class ResolvedUrl
 
 }
 
+/// <summary>Works as a store for registered routes. Can be used to check if requested route matches
+/// any of the registered route patterns. If so, it can provide a ResolveRoute object containing the 
+/// named parameters and their associated values</summary>
 public class RouteResolver
 {
-    public Dictionary<HTTPMethod, List<string>> routePatterns;
+    public Dictionary<HTTPMethod, List<string>> parsedRouteTemplates;
     private IUrlParser urlParser;
+
+    /// <summary>Constructor</summary>
+    /// <param name="urlParser">Requires a parser that implements the interface IUrlParser</param>
     public RouteResolver(IUrlParser urlParser)
     {
         this.urlParser = urlParser;
-        routePatterns = new Dictionary<HTTPMethod, List<string>>
+        parsedRouteTemplates = new Dictionary<HTTPMethod, List<string>>
         {
           { HTTPMethod.GET, new List<string>() },
           { HTTPMethod.POST, new List<string>() },
@@ -79,28 +98,69 @@ public class RouteResolver
         };
     }
 
+    /// <summary>Register a route template pattern using HTTP method GET</summary>
+    /// <param name="routePattern">
+    /// URL template for example like:
+    /// - /api/without/named/params
+    /// - /api/user/{userid:int} -> userid can only be an integer
+    /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+    /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+    /// </param>
     public void RegisterGet(string routePattern)
     {
-        this.routePatterns[HTTPMethod.GET].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
+        this.parsedRouteTemplates[HTTPMethod.GET].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
     }
+    /// <summary>Register a route template pattern using HTTP method POST</summary>
+    /// <param name="routePattern">
+    /// URL template for example like:
+    /// - /api/without/named/params
+    /// - /api/user/{userid:int} -> userid can only be an integer
+    /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+    /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+    /// </param>
     public void RegisterPost(string routePattern)
     {
-        this.routePatterns[HTTPMethod.POST].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
+        this.parsedRouteTemplates[HTTPMethod.POST].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
     }
+    /// <summary>Register a route template pattern using HTTP method PUT</summary>
+    /// <param name="routePattern">
+    /// URL template for example like:
+    /// - /api/without/named/params
+    /// - /api/user/{userid:int} -> userid can only be an integer
+    /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+    /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+    /// </param>
     public void RegisterPut(string routePattern)
     {
-        this.routePatterns[HTTPMethod.PUT].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
+        this.parsedRouteTemplates[HTTPMethod.PUT].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
     }
+    /// <summary>Register a route template pattern using HTTP method DELETE</summary>
+    /// <param name="routePattern">
+    /// URL template for example like:
+    /// - /api/without/named/params
+    /// - /api/user/{userid:int} -> userid can only be an integer
+    /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+    /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+    /// </param>
     public void RegisterDelete(string routePattern)
     {
-        this.routePatterns[HTTPMethod.DELETE].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
+        this.parsedRouteTemplates[HTTPMethod.DELETE].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
     }
 
+    /// <summary>Register a route template pattern.</summary>
+    /// <param name="method">Expects the HTTP method for which the route is registered for.</param>
+    /// <param name="routePattern">
+    /// URL template for example like:
+    /// - /api/without/named/params
+    /// - /api/user/{userid:int} -> userid can only be an integer
+    /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+    /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+    /// </param>
     public void RegisterRoute(string routePattern, HTTPMethod method)
     {
         if (Enum.IsDefined<HTTPMethod>(method))
         {
-            this.routePatterns[method].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
+            this.parsedRouteTemplates[method].Add(this.urlParser.ReplaceTokensWithRegexPatterns(routePattern));
         }
         else
         {
@@ -114,13 +174,16 @@ public class RouteResolver
     // was soll Map eigentlich genau machen?
     // IMPROVE potentialPatterns umbenennen
     // ? was soll diese methode überhaupt machen
+    /// <summary>Checks if the requested route is registered in the store.</summary>
+    /// <returns>ResolvedUrl object containing (in case they exist) values of the named url parameters 
+    /// and a bool (IsRouteRegistered) inidicating if the requested route was even registered in the store.</returns>
     public ResolvedUrl? MapRequest(string requestedUrl, HTTPMethod method)
     {
         Dictionary<string, string> namedTokensInUrlFound = new();
         List<string>? potentialPatterns;
         var res = new ResolvedUrl(method, requestedUrl);
 
-        if (!routePatterns.TryGetValue(method, out potentialPatterns))
+        if (!parsedRouteTemplates.TryGetValue(method, out potentialPatterns))
             return new ResolvedUrl(method, requestedUrl);
 
         string trimmedUrl = urlParser.CleanUrl(requestedUrl);
--- src/UrlParser/IUrlParser.cs
+++ src/UrlParser/IUrlParser.cs
@@ -3,10 +3,30 @@ using System.Text.RegularExpressions;
 
 namespace MTCG
 {
+    /// <summary>Interface for URL parsers.</summary>
     public interface IUrlParser
     {
+        /// <summary>Perform trimming and other necessary preparations of the raw url string for further parsing</summary>
+        /// <returns>Prepared url string for further processing/parsing</returns>
         string CleanUrl(string url);
+        /// <summary>
+        /// Replaces the variable parameters in the URL template with regex patterns so that named
+        /// tokens can be accessed by their name.
+        /// </summary>
+        /// <param name="urlTemplate">
+        /// URL template for example like:
+        /// - /api/without/named/params
+        /// - /api/user/{userid:int} -> userid can only be an integer
+        /// - /api/user/{username:alpha} -> username can only be letters from the alphabet
+        /// - /api/token/{accesstoken:alphanum} -> accesstoken can be any string
+        /// </param>
+        /// <returns>The URL template with replaced regex patterns.</returns>
         string ReplaceTokensWithRegexPatterns(string urlTemplate);
+        
+        /// <summary>Performs regex match to check if requested url matches provided pattern urlPattern</summary>
+        /// <param name="urlPattern">Regex pattern (string)</param>
+        /// <param name="url">Requested URL string. should be preprocessed with CleanUrl</param>
+        /// <returns>Dictionary with param names as key and value as its value.</returns>
         Dictionary<string, string> MatchUrlAndGetParams(string url, string urlPattern);
     }
 }

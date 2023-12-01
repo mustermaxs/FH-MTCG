5816b9b40d1cd9340e1b3d7229e7152c52cc4be2
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-01 10:32:53 +0100

    add extension of MethodBase.Invoke

---
+++ src/CustomReflectionExtension.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Reflection;
+
+namespace MTCG;
+
+    /// <summary>
+    /// Extension of System.Reflection MethodBase.Invoke
+    /// Used for passing the named parameters from URL to controller methods
+    /// in the right order
+    /// </summary>
+public static class CustomReflectionExtension
+{
+    public static T MapArgumentsAndInvoke<T>(this MethodBase self, object classInstance, Dictionary<string, object> providedParams)
+    {
+        return self.Invoke(classInstance, MapProvidedArgumentsWithSignature(providedParams));
+    }
+
+    public static object[] MapProvidedArgumentsWithSignature(MethodBase classInstanceMethod, Dictionary<string, object> providedParams)
+    {
+        string[] expectedParamNames = classInstanceMethod.GetParameters().Select(param => param.Name).ToArray();
+        object[] parameters = new object[expectedParamNames.Length];
+
+        for (int i = 0; i < parameters.Length; ++i)
+        {
+            parameters[i] = Type.Missing;
+        }
+        foreach (var param in providedParams)
+        {
+            var providedName = param.Key;
+            var paramIndex = Array.IndexOf(expectedParamNames, providedName);
+
+            if (paramIndex >= 0)
+            {
+                parameters[paramIndex] = param.Value;
+            }
+        }
+        return parameters;
+    }
+}

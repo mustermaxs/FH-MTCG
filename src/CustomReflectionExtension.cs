e6fe7cf83a0436ebbfe980878ac1607e633d4189
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-01 10:59:09 +0100

    fix bug

--- src/CustomReflectionExtension.cs
+++ src/CustomReflectionExtension.cs
@@ -3,16 +3,23 @@ using System.Reflection;
 
 namespace MTCG;
 
-    /// <summary>
-    /// Extension of System.Reflection MethodBase.Invoke
-    /// Used for passing the named parameters from URL to controller methods
-    /// in the right order
-    /// </summary>
+/// <summary>
+/// Extension of System.Reflection MethodBase.Invoke
+/// Used for passing the named parameters from URL to controller methods
+/// in the right order
+/// </summary>
 public static class CustomReflectionExtension
 {
     public static T MapArgumentsAndInvoke<T>(this MethodBase self, object classInstance, Dictionary<string, object> providedParams)
     {
-        return self.Invoke(classInstance, MapProvidedArgumentsWithSignature(providedParams));
+        if (providedParams.Count == 0)
+        {
+            return (T)self.Invoke(classInstance, null);
+        }
+        else
+        {
+            return (T)self.Invoke(classInstance, MapProvidedArgumentsWithSignature(self, providedParams));
+        }
     }
 
     public static object[] MapProvidedArgumentsWithSignature(MethodBase classInstanceMethod, Dictionary<string, object> providedParams)

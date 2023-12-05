e3b4c4502e58023404eb6a3e98515be61762c53a
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-05 10:24:21 +0100

    errorresponse without payload

--- src/ErrorResponse.cs
+++ src/ErrorResponse.cs
@@ -5,8 +5,8 @@ namespace MTCG;
 
 public class ErrorResponse<T> : BaseResponse<T>, IResponse where T : class?
 {
-    public ErrorResponse(T payload, string description)
-    :base(500, payload, description)
+    public ErrorResponse(string description, int statusCode = 500)
+    :base(statusCode, null, description)
     {
     }
 }

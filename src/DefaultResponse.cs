199f6f3b61a378deb9c836782156a5df1c2c358b
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-06 21:53:07 +0100

    obsolete DefaultResponse

---
+++ src/DefaultResponse.cs
@@ -0,0 +1,18 @@
+// using System;
+
+// namespace MTCG;
+
+/// 06.12.2023 21:45
+/// OBSOLETE
+
+// public class DefaultResponse : IResponse
+// {
+//     public int StatusCode { get; }
+//     public string Description {get;}
+
+//     public DefaultResponse(int statusCode, string description)
+//     {
+//         this.StatusCode = statusCode;
+//         this.Description = description;
+//     }
+// }

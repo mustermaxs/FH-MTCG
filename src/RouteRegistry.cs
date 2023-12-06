bfbda951d6f63abbe7e710fbadc48192f1a2a078
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-06 20:00:25 +0100

    add docu

--- src/RouteRegistry.cs
+++ src/RouteRegistry.cs
@@ -2,25 +2,56 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.RegularExpressions;
 
 namespace MTCG;
+
+
+
 
 /// <summary>
 /// Works as a store for registered routes. Can be used to 
 /// check if requested route matches
 /// any of the registered route patterns. 
-/// If so, it can provide a ResolveRoute object containing the 
-/// named parameters and their associated values
+/// If so, it provides informations such as which controller and which
+/// controller method to delegate the processing of the client request to.
 /// </summary>
-/// TODO/10 IMPORTANT add Payload from HttpSvrEventArgs to Endpoint somehow
+
 public class RouteRegistry : IEndpointMapper
 {
+
+
+
+
+    /// <summary>
+    /// Stores a dictionary of all the registered endpoints in the application.
+    /// Associates them with their HTTP-method for faster lookup and structure.
+    /// </summary>
+    
     private Dictionary<HTTPMethod, List<IEndpoint>> endpointMappings;
     private IUrlParser parser;
+
+
+
+    /// <summary>
+    /// Stores a static instance of a RouteRegistry.
+    /// </summary>
+    
     private static IEndpointMapper? _this = null;
 
-    // TODO Dependency injection geht so nicht mehr
+
+
+
+    /// <summary>
+    /// Return an instance of RouteRegistry class.
+    /// Only one instance should exist since it registeres all the routes
+    /// and should stay the same as long as the server is running.
+    /// </summary>
+    /// <param name="urlParser">
+    /// Parser to extract (named) variables from the requested URL
+    /// </param>
+
     public static IEndpointMapper GetInstance(IUrlParser urlParser)
     {
         if (_this == null)
@@ -29,6 +60,16 @@ public class RouteRegistry : IEndpointMapper
         }
         return _this;
     }
+
+
+    /// <summary>
+    /// Constructor. Is private since only a single instance
+    /// should be created.
+    /// </summary>
+    /// <param name="urlParser">
+    /// Parser to extract (named) variables from the requested URL
+    /// </param>
+    
     private RouteRegistry(IUrlParser urlParser)
     {
         this.parser = urlParser;
@@ -41,9 +82,28 @@ public class RouteRegistry : IEndpointMapper
                 { HTTPMethod.PATCH, new List<IEndpoint>() }
             };
     }
+
+
+
     public Dictionary<HTTPMethod, List<IEndpoint>> RegisteredEndpoints => this.endpointMappings;
 
-    //TODO
+
+
+
+    /// <summary>
+    /// Checks if a route template is already registered.
+    /// </summary>
+    /// <param name="routeTemplate">
+    /// Route template as defined via the route attributes
+    /// </param>
+    /// <param name="httpMethod">
+    /// The HTTPMethod for which the route template in question is presumably
+    /// registered (or not).
+    /// </param>
+    /// <returns>
+    /// Boolean
+    /// </returns>  
+    
     public bool IsRouteRegistered(string routeTemplate, HTTPMethod httpMethod)
     {
         List<IEndpoint> potentialEndpoints;
@@ -55,6 +115,25 @@ public class RouteRegistry : IEndpointMapper
 
         return false;
     }
+
+
+
+    /// <summary>
+    /// Adds an endpoint to the registry.
+    /// </summary>
+    /// <param name="routePattern">
+    /// Route template (e.g. IUrlParser.cs).
+    /// </param>
+    /// <param name="method">
+    /// HTTPMethod for which the endpoint should be defined for.
+    /// </param>
+    /// <param name="controllerType">
+    /// Type of the controller that's supposed to handle the request.
+    /// </param>
+    /// <param name="controllerMethod">
+    /// Controller method that's supposed to handle the request.
+    /// </param>
+    
     public void RegisterEndpoint(string routePattern, HTTPMethod method, Type controllerType, MethodInfo controllerMethod)
     {
         var parsedRoutePattern = this.parser.ReplaceTokensWithRegexPatterns(routePattern);
@@ -70,6 +149,16 @@ public class RouteRegistry : IEndpointMapper
                 "{HTTPMethod.GET}, {HTTPMethod.POST}, {HTTPMethod.PUT}, {HTTPMethod.DELETE}, {HTTPMethod.PATCH}.");
         }
     }
+
+
+
+    /// <summary>
+    /// Registeres an endpoint with a given IEndpoint object
+    /// </summary>
+    /// <param name="endpoint">
+    /// Object implementing the IEndpoint interface
+    /// </param>
+    
     public void RegisterEndpoint(IEndpoint endpoint)
     {
         var parsedRoutePattern = this.parser.ReplaceTokensWithRegexPatterns(endpoint.RouteTemplate);
@@ -92,15 +181,26 @@ public class RouteRegistry : IEndpointMapper
         }
     }
 
+
+
+
     /// <summary>
-    /// Checks if the requested route is registered in the store.
+    /// Upon receiving a request for a specific endpoint, this method
+    /// tries to map the requested URL and HTTPMethod to one of the registered
+    /// endpoints.
     /// </summary>
+    /// <param name="requestedUrl">
+    /// The requested URL
+    /// </param>
+    /// <param name="method">
+    /// The requested HTTP method
+    /// </param>
     /// <returns>
-    /// ResolvedUrl object containing (in case they exist)
-    /// values of the named url parameters 
-    /// and a bool (IsRouteRegistered) inidicating 
-    /// if the requested route was even registered in the store.
+    /// In case the requested endpoint can be found in the registry,
+    /// this method returns an object that implements the IEndpoint interface.
+    /// If it doesn't find a matching endpoint, it throws a RouteDoesntExistException.
     /// </returns>
+    
     public IEndpoint? MapRequestToEndpoint(string requestedUrl, HTTPMethod method)
     {
         List<IEndpoint> potentialEndpoints = endpointMappings[method];
@@ -129,6 +229,24 @@ public class RouteRegistry : IEndpointMapper
         return null;
     }
 
+
+
+
+    /// <summary>
+    /// Upon receiving a request for a specific endpoint, this method
+    /// tries to map the requested URL and HTTPMethod to one of the registered
+    /// endpoints.
+    /// </summary>
+    /// <param name="ref context">
+    /// Uses a reference to a RoutingContext object to gather all the
+    /// necessary information concerning the clients request.
+    /// </param>
+    /// <returns>
+    /// void. Stores all gathered information about the endpoint directly
+    /// inside the RoutingContext object.
+    /// Throws a RouteDoesntExistException in case the requested endpoint wasn't found.
+    /// </returns>
+    
     public void MapRequestToEndpoint(ref RoutingContext context)
     {
         List<IEndpoint> potentialEndpoints = endpointMappings[context.httpMethod];
@@ -148,7 +266,6 @@ public class RouteRegistry : IEndpointMapper
                 return;
             }
         }
-
 
         if (!context.RouteFound)
         {

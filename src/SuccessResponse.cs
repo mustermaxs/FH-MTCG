01eb1cd13f6f241dd6b81c2445c5502abca4760f
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-06 21:57:03 +0100

    rename BaseResponse->PayloadResponse

--- src/SuccessResponse.cs
+++ src/SuccessResponse.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace MTCG;
 
-public class SuccessResponse<T> : BaseResponse<T> where T : class?
+public class SuccessResponse<T> : PayloadResponse<T> where T : class?
 {
     public SuccessResponse(T payload, string description)
         : base(200, payload, description)

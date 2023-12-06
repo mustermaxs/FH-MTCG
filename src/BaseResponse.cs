48203c10c365b07705bdd5696b953bc53bad0954
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-06 21:52:56 +0100

    rename BaseResponse -> PayloadResponse

--- src/BaseResponse.cs
+++ src/BaseResponse.cs
@@ -3,20 +3,20 @@ using System.Text.Json;
 
 namespace MTCG;
 
-public abstract class BaseResponse<T> : IResponse where T : class?
+public abstract class PayloadResponse<T> : IResponse where T : class?
 {
     public T? Payload { get; }
     virtual public int StatusCode { get; }
     virtual public string Description { get; }
 
-    public BaseResponse(int statusCode, T? payload, string? description)
+    public PayloadResponse(int statusCode, T? payload, string? description)
     {
         this.Payload = payload;
         this.StatusCode = statusCode;
         this.Description = description ?? string.Empty;
     }
 
-    public string PayloadAsJson()
+    virtual public string PayloadAsJson()
     {
         if (Payload == null)
         {
--- src/CustomResponse.cs
+++ src/CustomResponse.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace MTCG;
 
-public class CustomResponse<T> : BaseResponse<T> where T : class?
+public class CustomResponse<T> : PayloadResponse<T> where T : class?
 {
     public CustomResponse(int statusCode, T? payload, string description)
     :base(statusCode, payload, description)

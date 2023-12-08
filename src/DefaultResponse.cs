1991a0a1197871cf4c0522d333e21052adee41f0
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-08 12:07:34 +0100

    add TextReponse

--- src/DefaultResponse.cs
+++ src/DefaultResponse.cs
@@ -5,16 +5,20 @@ namespace MTCG;
 /// 06.12.2023 21:45
 /// OBSOLETE
 
-public class Response : IResponse
+public class TextResponse : IResponse
 {
     public int StatusCode { get; }
-    public string Description {get;}
+    public string Description { get; }
+    public string ContentType { get; } = "text/html";
 
-    public Response(int statusCode, string description)
+
+
+    public TextResponse(int statusCode, string description)
     {
         this.StatusCode = statusCode;
         this.Description = description;
     }
+
 
 
     virtual public string PayloadAsJson() => string.Empty;

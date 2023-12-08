7b60b9c03490713aa1c71ec283fa4208a1cd799f
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-08 12:35:40 +0100

    add comments

--- src/Program.cs
+++ src/Program.cs
@@ -15,18 +15,23 @@ namespace MTCG
         /// <param name="args">Arguments.</param>
         static void Main(string[] args)
         {
+
+
+            /// <summary>
+            /// Setup necessary entities for mapping of endpoints,
+            /// instantiating and starting server and router.
+            /// This application currently uses reflection and custom attributes
+            /// to register endpoints and the responsible controllers
+            /// to handle requests.
+            /// </summary>
             IUrlParser urlParser = new UrlParser();
             IEndpointMapper routeRegistry = RouteRegistry.GetInstance(urlParser);
             IAttributeHandler attributeHandler = new AttributeHandler();
             IRouteObtainer routeObtainer = new ReflectionRouteObtainer(attributeHandler);
             
-            var router = new Router(routeRegistry, routeObtainer);
+            Router router = new(routeRegistry, routeObtainer);
             HttpServer svr = new(router);
             svr.Run();
-
-            // ConstructorInfo info = typeof(User).GetConstructors()[0];
-            // User user = info.MapArgumentsAndCreateInstance<User>();
-            // Console.WriteLine("test");
         }
     }
 }

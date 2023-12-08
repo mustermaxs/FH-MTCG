7cfba9fded32d7a6d5d9ed8b3803d0da9c020d17
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-08 12:30:26 +0100

    rename base class

--- src/ErrorResponse.cs
+++ src/ErrorResponse.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 
 namespace MTCG;
 
-public class ErrorResponse<T> : PayloadResponse<T>, IResponse where T : class?
+public class ErrorResponse<T> : BaseJsonResponse<T>, IResponse where T : class?
 {
     public ErrorResponse(string description, int statusCode = 500)
     :base(statusCode, null, description)

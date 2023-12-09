45023bea5fec581199615e651c36f8db102ad557
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-09 14:35:50 +0100

    draft tokenvalidator&sessionmngr

--- src/IAuthenticator.cs
+++ src/IAuthenticator.cs
@@ -2,7 +2,33 @@ using System;
 
 namespace MTCG;
 
-public interface IAuthenticator
+public interface ITokenValidator
 {
     public bool ValidateToken(string token);
+    public bool TokenIsValid(string token);
+    public User GetClientModel();
+}
+
+public abstract class Session
+{
+
+}
+
+public abstract class BaseSessionManager
+{
+    ///? wie session id generieren
+    /// session id in request object speichern,
+    /// sessions field **static** speichern
+    ///? oder in request session speichern?
+    public BaseSessionManager()
+    {
+        
+    }
+    
+    private static Dictionary<string, Session> sessions;
+    abstract Session CreateSession(IRequest request);
+    virtual public Session GetSessionById(string sessionId);
+
+
+
 }

150ac3e0efca60ebee605762d2da54e1195b707b
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-09 13:40:01 +0100

    refactor & test ConvertToType

--- MTCG.Tests/UnitTest1.cs
+++ MTCG.Tests/UnitTest1.cs
@@ -166,6 +166,30 @@ public class ControllerTests
     }
 }
 
+[TestFixture]
+public class Test_ReflectionUtils
+{
+    public static IEnumerable<object[]> ConvertToTypeTestCases()
+    {
+        yield return new object[] { "897cb65a-4381-4c14-afda-65ff2cd291a4", typeof(Guid), new Guid("897cb65a-4381-4c14-afda-65ff2cd291a4") };
+        // Add more test cases here
+    }
+
+    [TestCaseSource(nameof(ConvertToTypeTestCases))]
+    [TestCase("23", typeof(int), (int)23)]
+    [TestCase("abc", typeof(string), (string)"abc")]
+    [TestCase("true", typeof(bool), (bool)true)]
+    [TestCase("false", typeof(bool), (bool)false)]
+    [TestCase("false", typeof(bool), (bool)false)]
+    [TestCase("3.1415", typeof(float), (float)3.1415)]
+    public void Test_ConvertStringToExpectedTypes(string provided, Type expectedType, dynamic expectedValue)
+    {
+        var res = ReflectionUtils.ConvertToType(provided, expectedType);
+
+        Assert.That(res == expectedValue, $"Converting string to {expectedType} failed.\nProvided: {provided}\nExpected: {expectedValue}");
+    }
+}
+
 
 //    [TestFixture]
 //     public class RouterTests

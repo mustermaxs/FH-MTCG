db0f7620fc55939e5b03d8d83c598e4bc37ffbf9
Maximilian Sinnl <max.sinnl@hotmail.com>
2023-12-11 17:42:03 +0100

    add comments

--- src/SessionManager.cs
+++ src/SessionManager.cs
@@ -3,19 +3,45 @@ using Npgsql.Replication;
 
 namespace MTCG;
 
+
+
+
+/// <summary>
+/// Stores each created session in a static
+/// dictionary.
+/// The inidivual sessions provide client
+/// details (if logged in) among other things.
+/// </summary>
 public class SessionManager : BaseSessionManager
 {
+
+
+
+    /// <summary>
+    /// Creates a new session object and stores it
+    /// in a static dictionary.
+    /// </summary>
+    /// <param name="sessionId"></param>
+    /// <param name="client"></param>
+    /// <returns></returns>
     public static string CreateSession(string sessionId, User client)
     {
         var session = new Session(sessionId, client);
         Sessions.Add(sessionId, session);
+        
         return sessionId;
     }
+
+
+
 
     public static void EndSession(string sessionId)
     {
         Sessions.Remove(sessionId);
     }
+
+
+
 
     public static bool TryGetSession(string sessionId, out Session session)
     {

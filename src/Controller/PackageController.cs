6a795b1fdc19290e040c28da7aeabf6b87de07ce
Maximilian Sinnl <max.sinnl@hotmail.com>
2024-01-05 20:05:57 +0100

    get packages

--- src/Controller/PackageController.cs
+++ src/Controller/PackageController.cs
@@ -8,11 +8,12 @@ public class PackageController : IController
 {
     protected static PackageRepository repo = new PackageRepository();
     public PackageController(IRequest request) : base(request) { }
+    const int MIN_COINS_NORMAL_PACKAGE = 5;
 
     //////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////
 
-    [Route("/packages", HTTPMethod.POST, Role.USER)]
+    [Route("/packages", HTTPMethod.POST, Role.USER)] // CHANGE only ADMIN
     public IResponse AddPackage()
     {
         try
@@ -37,6 +38,68 @@ public class PackageController : IController
 
             return new Response<string>(403, "At least one card in the packages already exists");
         }
+    }
+
+
+    [Route("/packages", HTTPMethod.GET, Role.ALL)]
+    public IResponse GetAvailablePackages()
+    {
+        try
+        {
+            IEnumerable<Package> packages = repo.GetAll();
+
+            if (packages.Count() == 0) return new Response<string>(204, "The request was fine, but there are no packages");
+
+            return new Response<IEnumerable<Package>>(200, packages, $"The response contains all packages");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+
+            return new Response<string>(500, "Internal server error. Something went wrong :(");
+        }
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
+    [Route("/packages/{packageid:alphanum}", HTTPMethod.GET, Role.ALL)]
+    public IResponse GetPackageById(Guid packageid)
+    {
+        try
+        {
+            Package? package = repo.Get(packageid);
+
+            if (package == null) return new Response<string>(204, "The request was fine, but the package doesn't exist");
+
+            return new Response<Package>(200, package, $"The response contains the package");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+
+            return new Response<string>(500, "Internal server error. Something went wrong :(");
+        }
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    
+    
+    [Route("/transactions/packages", HTTPMethod.POST, Role.USER)]
+    public IResponse UserBuysPackage()
+    {
+        throw new NotImplementedException();
+        // var user = LoggedInUser;
+        // Guid? packageId = repo.GetAllPackageIds().FirstOrDefault();
+
+        // if (packageId == Guid.Empty || packageId == null)
+        //     return new Response<string>(404, "No card package available for buying");
+        
+        // if (user.Coins >= 5)
+        
     }
 }
 //////////////////////////////////////////////////////////////////////
--- src/Repository/PackageRepository.cs
+++ src/Repository/PackageRepository.cs
@@ -12,18 +12,128 @@ public class PackageRepository : BaseRepository<Package>, IRepository<Package>
         _Table = "packages";
         _Fields = "*";
     }
+
     public override IEnumerable<Package> GetAll()
     {
-        ObjectBuilder<Package> objectBuilder = Fill;
+        var cardRepo = new CardRepository();
+        List<Package> packages = new List<Package>();
+        List<Guid> packageIds = GetAllPackageIds().ToList();
+        List<Card> packageCards = new List<Card>();
+
+        foreach (Guid packageId in packageIds)
+        {
+            List<Guid> packageCardIds = GetPackageCardIds(packageId).ToList();
+            var package = new Package();
+            package.Id = packageId;
+            package.CardIds = packageCardIds;
+            List<Card> cards = cardRepo.GetCardsByIds(packageCardIds);
+            package.Cards = cards;
+            packages.Add(package);
+        }
+
+        return packages;
+    }
+
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+
+    /// <summary>
+    /// Gets all cards that are contained in a package.
+    /// </summary>
+    /// <param name="packageCardIds"></param>
+    /// <param name="cardRepo"></param>
+    /// <returns>
+    /// List of cards.
+    /// </returns>
+    /// <exception cref="Exception">
+    /// Thrown when a card is not found.
+    /// </exception>
+    private List<Card> GetCardsForPackage(List<Guid> packageCardIds)
+    {
+        var cardRepo = new CardRepository();
+        List<Card> cards = new List<Card>();
+
+        foreach (Guid cardId in packageCardIds)
+        {
+            var card = cardRepo.Get(cardId) ?? throw new Exception($"Failed to get all cards for package.\nCardId {cardId}");
+            cards.Add(card);
+        }
+
+        return cards;
+    }
+
+
+    /// <summary>
+    /// Gets all ids of all available packages.
+    /// </summary>
+    /// <returns>
+    /// IEnumerable of all package ids.
+    /// </returns>
+    public IEnumerable<Guid> GetAllPackageIds()
+    {
+        ObjectBuilder<ValueTypeWrapper<Guid>> objectBuilder =
+            (ValueTypeWrapper<Guid> v, IDataReader re) => v.Value = re.GetGuid(re.GetOrdinal("id"));
+            
         var builder = new QueryBuilder(this.Connect());
         builder
-            .Select("p.id", "pc.cardid", "pc.packageid")
+            .Select("id")
+            .From("packages")
+            .Build();
+
+        var list = builder.ReadMultiple<ValueTypeWrapper<Guid>>(objectBuilder);
+
+        return list.Select(e => e.Value);
+    }
+
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// Gets all card ids for a given package id.
+    /// </summary>
+    /// <param name="packageId">
+    /// The package id.
+    /// </param>
+    /// <returns>
+    /// IEnumerable of all card ids for the given package id.
+    /// </returns>
+    protected IEnumerable<Guid> GetPackageCardIds(Guid packageId)
+    {
+        ObjectBuilder<ValueTypeWrapper<Guid>> objectBuilder = (ValueTypeWrapper<Guid> v, IDataReader re) => v.Value = re.GetGuid(re.GetOrdinal("cardid"));
+        var builder = new QueryBuilder(this.Connect());
+        builder
+            .Select("p.id", "pc.cardid as cardid", "pc.packageid")
             .From("packages p")
             .Join("packagecards pc")
             .On("pc.packageid=p.id")
+            .Where("p.id=@packageid")
+            .AddParam("@packageid", packageId)
             .Build();
 
-        return builder.ReadMultiple<Package>(objectBuilder);
+        var list = builder.ReadMultiple<ValueTypeWrapper<Guid>>(objectBuilder);
+
+        return list.Select(e => e.Value);
+    }
+
+    public override Package? Get(Guid id)
+    {
+        var packageId = GetAllPackageIds().SingleOrDefault<Guid>(p => p == id);
+
+        if (packageId == Guid.Empty) return null;
+
+        var cardRepo = new CardRepository();
+        var package = new Package();
+        var packageCardIds = GetPackageCardIds(packageId).ToList();
+        var cards = cardRepo.GetCardsByIds(packageCardIds);
+
+        package.Id = packageId;
+        package.CardIds = packageCardIds;
+        package.Cards = cards;
+
+        return package;
     }
 
     public override void Save(Package package)
@@ -54,26 +164,39 @@ public class PackageRepository : BaseRepository<Package>, IRepository<Package>
         builder
             .InsertInto("packages")
             .InsertValues()
-            .GetInsertedIds()
+            .GetInsertedIds(true)
             .Build();
 
-        IEnumerable<Guid> insertedIds = builder.Run<Guid>();
+        Guid? insertedId = builder.ReadSingle<Guid>("id");
 
-        return insertedIds.FirstOrDefault();
+        return insertedId ?? Guid.Empty;
     }
 
     protected override void Fill(Package obj, IDataReader re)
     {
-        throw new NotImplementedException();
+        obj.Id = re.GetGuid(re.GetOrdinal("id"));
     }
     protected override void _Fill(Package obj, IDataReader re)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Deletes a package from the database along with its
+    /// associated cards in the packagecards table.
+    /// </summary>
+    /// <param name="obj"></param>
     public override void Delete(Package obj)
     {
-        throw new NotImplementedException();
+        var builder = new QueryBuilder(this.Connect());
+
+        builder
+            .DeleteFrom("packages")
+            .Where("id=@id")
+            .AddParam("@id", obj.Id)
+            .Build();
+            
+            builder.ExecuteNonQuery();
     }
 
 

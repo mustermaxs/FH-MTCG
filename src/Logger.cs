767ec5223210e17d93c64e484425461e4ef0d0c2
Maximilian Sinnl <max.sinnl@hotmail.com>
2024-01-09 22:53:48 +0100

    add logger

---
+++ src/Logger.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace MTCG;
+
+public class Logger
+{
+    public readonly static string decoline = new string('-', 15);
+    public static void ToConsole(string txt, bool withTimeStamp=false)
+    {
+        DateTime localDate = DateTime.Now;
+        DateTime utcDate = DateTime.UtcNow;
+        var timestamp = withTimeStamp ? localDate.ToString("yyyy-MM-dd HH:mm:ss") : "";
+
+        string decoline = new string('-', 15);
+        string logTxt = $"{decoline}\n{timestamp}\n";
+        Console.WriteLine(logTxt);
+    }
+}

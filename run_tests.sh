1f2f0f2657bc7ec86e6a43d67aaf5023571886ac
Maximilian Sinnl <max.sinnl@hotmail.com>
2024-01-09 13:32:48 +0100

    Refactor run_tests.sh script to include options for running integration, unit, or both tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
 
-src/Integration_Tests/integration_tests.py
-cd MTCG.Tests && dotnet test && cd ..
+echo "############   RUNNING TESTS   ############"
+
+if [ "$#" -eq 0 ]; then
+    echo "[Error] Please provide an argument (integration, unit, both)"
+    exit 1
+fi
+
+run_integration_tests() {
+    echo -e "\n\n############   INTEGRATION TESTS   ############\n\n"
+    find src/Integration_Tests -name "integration_tests.py" -exec python3 {} \;
+}
+
+run_unit_tests() {
+    echo -e "\n\n############      UNIT TESTS       ############\n\n"
+    cd MTCG.Tests && find . -name "*.csproj" -exec dotnet test {} \; && cd ..
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i|--integration)
+            run_integration_tests
+            ;;
+        -u|--unit)
+            run_unit_tests
+            ;;
+        -b|--both)
+            run_integration_tests
+            run_unit_tests
+            ;;
+        *)
+            echo "[Error] Unknown option $1. Use -i for 'integration', -u for 'unit', or -b for 'both'."
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+

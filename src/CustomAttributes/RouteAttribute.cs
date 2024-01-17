03abbb4221b0761a2c069d1b98ee1106dc476514
Maximilian Sinnl <max.sinnl@hotmail.com>
2024-01-17 10:19:31 +0100

    Add Role enum and update RouteAttribute class

--- src/CustomAttributes/RouteAttribute.cs
+++ src/CustomAttributes/RouteAttribute.cs
@@ -2,38 +2,45 @@ using System;
 using MTCG;
 
 
-namespace MTCG
+namespace MTCG;
+
+
+
+/// <summary>
+/// Client roles.
+/// </summary>
+[Flags]
+public enum Role
 {
-    [Flags]
-    public enum Role
+    ANONYMOUS = 1, // for clients who are not logged in
+    USER = 2,
+    ADMIN = 4,
+    ALL = ANONYMOUS | USER | ADMIN
+}
+
+
+/// <summary>
+/// Marks a (controller) method as the handler for a specific endpoint.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class RouteAttribute : Attribute
+{
+    private string routeTemplate;
+    private HTTPMethod method;
+    private Role accessLevel;
+
+    public RouteAttribute(string route, HTTPMethod method, Role accessLevel)
     {
-        ANONYMOUS = 1,
-        USER = 2,
-        ADMIN = 4,
-        ALL = ANONYMOUS | USER | ADMIN
+        this.routeTemplate = route;
+        this.method = method;
+        this.accessLevel = accessLevel;
     }
 
-
-    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
-    public class RouteAttribute : Attribute
+    public string RouteTemplate
     {
-        private string routeTemplate;
-        private HTTPMethod method;
-        private Role accessLevel;
-
-        public RouteAttribute(string route, HTTPMethod method, Role accessLevel)
-        {
-            this.routeTemplate = route;
-            this.method = method;
-            this.accessLevel = accessLevel;
-        }
-
-        public string RouteTemplate
-        {
-            get { return routeTemplate; }
-        }
-
-        public HTTPMethod Method => method;
-        public Role AccessLevel => accessLevel;
+        get { return routeTemplate; }
     }
+
+    public HTTPMethod Method => method;
+    public Role AccessLevel => accessLevel;
 }
